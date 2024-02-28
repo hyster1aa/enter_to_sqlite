e1fee47121a5d1920d8bb38049a3c0d4b559d67b
hyster1aa <mixail.demen.2002@gmail.com>
2024-02-28 23:12:40 +0500

    Форма с маршрутами

---
+++ enter_to_sqlite/UI/Forms/PathForm.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace enter_to_sqlite.UI.Forms
+{
+    partial class PathForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // PathForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "PathForm";
+            Text = "Управление маршрутами";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
