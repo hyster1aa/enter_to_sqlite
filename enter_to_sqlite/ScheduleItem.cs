766eab92e321d52ceb0185dba3b37947baa3731c
hyster1aa <mixail.demen.2002@gmail.com>
2024-03-01 19:31:58 +0500

    checker

--- enter_to_sqlite/ScheduleItem.cs
+++ enter_to_sqlite/ScheduleItem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +8,17 @@ namespace enter_to_sqlite
 {
     public class ScheduleItem
     {
-        public int id { get; set; }
+        public int id_travel { get; set; }
+        public int id_train { get; set;  }
         public string typeTrain { get; set; }
         public Routes routes { get; set; }
         public string timeStart { get; set; }
         public string dateStart { get; set; }
         public string timeTravel { get; set; }
-        public ScheduleItem(int id,string typeTrain, Routes routes, string timeStart, string dateStart, string timeTravel)
+        public ScheduleItem(int id_travel, int id_train, string typeTrain, Routes routes, string timeStart, string dateStart, string timeTravel)
         {
-            this.id = id;
+            this.id_travel = id_travel;
+            this.id_train = id_train;
             this.typeTrain = typeTrain;
             this.routes = routes;
             this.timeStart = timeStart;
