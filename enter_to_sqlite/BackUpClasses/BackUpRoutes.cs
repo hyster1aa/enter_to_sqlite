3ab6bcd3635864dc4a6eb7db14fe10c87e5de578
hyster1aa <mixail.demen.2002@gmail.com>
2024-03-01 20:16:05 +0500

    А почему она закинулась в гитигноре ааа????

---
+++ enter_to_sqlite/BackUpClasses/BackUpRoutes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enter_to_sqlite.BackUpClasses
+{
+    public class BackUpRoutes
+    {
+        public int id_route { get; set; }
+        public int dep_point { get; set; }
+        public int arr_point { get; set; }
+        public BackUpRoutes(int id_route, int dep_point, int arr_point) { 
+            this.id_route = id_route;
+            this.dep_point = dep_point;
+            this.arr_point = arr_point;
+        }
+    }
+}
---
+++ enter_to_sqlite/BackUpClasses/BackUpSchedule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enter_to_sqlite.BackUpClasses
+{
+    public class BackUpSchedule
+    {
+        public int id_travel { get; set; }
+        public int id_train { get; set; }
+        public int id_route { get; set; }
+        public string type_train { get; set; }
+
+        public string timeStart { get; set; }
+        public string timeTravel { get; set; }
+        public string dateStart { get; set; }
+        public BackUpSchedule(int id_travel, int id_train, int id_route, string type_train, string timeStart, string timeTravel, string dateStart)
+        {
+            this.id_travel = id_travel;
+            this.id_train = id_train;
+            this.id_route = id_route;
+            this.type_train = type_train;
+            this.timeStart = timeStart;
+            this.timeTravel = timeTravel;
+            this.dateStart = dateStart;
+        }
+    }
+}
